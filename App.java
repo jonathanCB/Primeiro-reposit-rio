192a85a532d0aab1e31524c34a3307c7679134da
Jonathan Cardarelli <jonas.ca2@gmail.com>
2020-03-16 10:22:48 -0300

    a primeira versão foi adicionada

---
+++ App.java
@@ -0,0 +1,14 @@
+//Primeira versão
+public class App{
+    public static void main(String args[]){
+        Placa p1 = new Placa("Italia","ABC1R23");
+        
+        Veiculo v = new Veiculo(p1);
+
+        v.abastece(65);
+        v.dirige(250);
+        System.out.println("Dados do veiculo:");
+        System.out.println(v);
+        System.out.println("----------------");
+        }
+}
